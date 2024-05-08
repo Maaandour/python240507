8213097ea720b38aba98807f949bf276eec6f8eb
Maaandour <kms5410@gmail.com>
2024-05-08 11:08:07 +0900

    클래스 연습 완료

--- Person2.py
+++ Person2.py
@@ -1,4 +1,5 @@
 # Person2.py 
+#1)클래스 형식 정의
 class Person:
     #클래스 멤버 변수
     num_person = 0 
@@ -8,9 +9,11 @@ class Person:
     def print(self):
         print("My name is {0}".format(self.name))
 
-
+#2)인스턴스 생성
 p1 = Person()
 p2 = Person()
+
+#3)메소트 호출
 print("숫자:{0}".format(Person.num_person))
 Person.title = "new title"
 print(p1.title)
